74326fbe2d0b6494fdf5241814f8c2358e352081
Mattias Rydengren <mattias.rydengren@coderesque.com>
2015-02-13 10:28:17 +0100

    Add 'sample' for bundling AngularJS templates in a JavaScript file.

---
+++ src/Templar.Web/Samples/angular/AngularTemplateSource.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web;
+
+namespace Templar.Web.Samples.angular
+{
+    public class AngularTemplateSource : FileContentSource
+    {
+        private readonly TemplateFinder finder;
+
+        public AngularTemplateSource(string virtualPath, string searchPattern)
+        {
+            finder = new TemplateFinder(virtualPath, searchPattern);
+        }
+
+        protected override Dictionary<string, string> GetSubstitutions(HttpContextBase httpContext)
+        {
+            using (var writer = new StringWriter())
+            {
+                var templates = finder.Find(httpContext);
+                foreach (var template in templates)
+                {
+                    string name = template.GetName() + ".html";
+                    string content = HttpUtility.JavaScriptStringEncode(template.GetContent());
+
+                    writer.WriteLine("        $templateCache.put('{0}', '{1}');", name, content);
+                }
+
+                return new Dictionary<string, string>
+                {
+                    { "'__templates__';", writer.ToString() }
+                };
+            }
+        }
+
+        protected override string GetRootPath()
+        {
+            return "~/Samples/angular";
+        }
+
+        protected override string GetFilename()
+        {
+            return "templates.js";
+        }
+    }
+}
---
+++ src/Templar.Web/Samples/angular/templates.js
@@ -0,0 +1,11 @@
+﻿!function(angular) {
+    'use strict';
+    
+    angular.module('stratsys.comparison.service')
+           .run(['$templateCache', templates]);
+
+    function templates($templateCache) {
+'__templates__';
+    }
+
+}(angular);
