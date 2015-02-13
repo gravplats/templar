217e28038906b33166bed795ef5fc964370f0137
Mattias Rydengren <mattias.rydengren@coderesque.com>
2015-02-13 10:20:51 +0100

    Base class for reading 'virtual' files from the file system.

---
+++ src/Templar/FileContentSource.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Templar
+{
+    public abstract class FileContentSource : IContentSource
+    {
+        protected abstract Dictionary<string, string> GetSubstitutions(HttpContextBase httpContext);
+
+        public string GetContent(HttpContextBase httpContext)
+        {
+            string filename = "", path = "";
+
+            try
+            {
+                string root = GetRootPath().TrimEnd('/');
+
+                filename = GetFilename();
+                path = httpContext.Server.MapPath(root + "/" + filename);
+
+                string content = File.ReadAllText(path);
+
+                return GetSubstitutions(httpContext)
+                    .Aggregate(content, (script, substitution) => script.Replace(substitution.Key, substitution.Value));
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex, path, filename);
+                throw;
+            }
+        }
+
+        protected abstract string GetRootPath();
+        protected abstract string GetFilename();
+
+        protected virtual void HandleException(Exception ex, string path, string filename) { }
+    }
+}
